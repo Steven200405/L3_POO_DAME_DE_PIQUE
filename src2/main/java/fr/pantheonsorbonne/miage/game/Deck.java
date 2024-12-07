b858a8d611582dbea4ab4a2c235611dd38bcf6cf
Steven200405 <huangsteven2004@gmail.com>
2024-12-07 23:16:15 +0000

    Add method in Deck: swap3Cards

--- src2/main/java/fr/pantheonsorbonne/miage/game/Deck.java
+++ src2/main/java/fr/pantheonsorbonne/miage/game/Deck.java
@@ -1,47 +1,83 @@
 package fr.pantheonsorbonne.miage.game;
 
-import Card;
+
+
+import fr.pantheonsorbonne.miage.Card;
 import java.util.Random;
 
-public class Deck extends Card{
+public class Deck extends Card {
     private final static int DECK_SIZE = 13;
-    private final static Card[] cards;
-    private int countDistributedCard = 0;
+    private static Card[] cards;
+    private static int countDistributedCard;
+    private int manche;
 
-static {
+    static {
+        countDistributedCard = 0;
         cards = new Card[52];
         Random rand = new Random();
         cards.mixCards();
-
     }
-    private static void mixCards(){
+
+    private static void mixCards() {
         int index = 0;
-        for(CardColor color: CardColor.values()){
-            for(CardValue value: CardValue.values()){
+        for (CardColor color : CardColor.values()) {
+            for (CardValue value : CardValue.values()) {
                 cards[index++] = new Card(value, color);
             }
         }
 
-        for (int j = 0; j < cards.length; j++){
+        for (int j = 0; j < cards.length; j++) {
             Card temp = cards[j];
             int indexAleatoire = rand.nextInt(0, cards.length);
             cards[j] = cards[indexAleatoire];
             cards[indexAleatoire] = temp;
-            }
+        }
     }
-   
-    private Card[] giveCards(int countRestCard){
-        Card[] handle = new Card[DECK_SIZE];
-        for (int i = countDistributedCard; i < cards.length; i++){
-                handle[i] = cards[i];
-                if(i % DECK_SIZE == 0){
-                    countDistributedCard = countDistributedCard + i;
-                    break;
+
+    public Card[] giveCards() {
+        Card[] hand = new Card[DECK_SIZE];
+        for (int i = countDistributedCard; i < cards.length; i++) {
+            handle[i] = cards[i];
+            if (i % DECK_SIZE == 0) {
+                countDistributedCard = countDistributedCard + DECK_SIZE;
+                break;
+            }
+        }
+        return hand;
+    }
+
+    public void swap3Cards(Card[] hand1, Card[] hand2) {
+        Card[] swapCardsHand1 = getSwap3Cards(hand1);
+        Card[] swapCardsHand2 = getSwap3Cards(hand2);
+
+        swapCards(hand1, swapCardsHand1, swapCardsHand2);
+        swapCards(hand2, swapCardsHand2, swapCardsHand1);
+
+    }
+
+    private Card[] getSwap3Cards(Card[] hand) {
+        Card[] cardsToGive = new Card[3];
+        Card maxCard = hand[0];
+        int index = 0;
+        while (index != 2) {
+            for (Card card : hand) {
+                if (card.getValue().getRank() > maxCard.getValue().getRank()) {
+                    maxCard = card;
                 }
             }
-            return handle;
+            cardsToGive[index++] = maxCard;
+        }
+        return cardsToGive;
     }
-}
-        
-    
 
+    private void swapCards(Card[] hand, Card[] swapCardsHand1, Card[] swapCardsHand2) {
+        for (int i = 0; i < hand.length; i++) {
+            for (int j = 0; j < swapCardsHand1.length; j++) {
+                if (hand[i].equals(swapCardsHand1[j])) {
+                    hand[i] = swapCardsHand2[j];
+                }
+            }
+        }
+    }
+
+}
